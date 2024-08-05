164480747c5b4f103e00f2194845b1f267ac04d9
LauraP <166152671+LauraPeckyno@users.noreply.github.com>
2024-08-05 15:39:38 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+  //--------------------
+  //dice roll
+  function rollDice() {
+    return Math.floor(Math.random() * 10) + 1;
+  }
+
+  //Select Player one
+  //https://scontent.fagc3-2.fna.fbcdn.net/o1/v/t0/f1/m255/upload_img_2367944_08_05_2024_12_11_57_383301_2756706575569245631.jpeg?_nc_ht=scontent.fagc3-2.fna.fbcdn.net&_nc_cat=109&ccb=9-4&oh=00_AYCfJ7dyYLJr_06Nb3rezTMH4EKvYQU6keYnpVDht_vQAg&oe=66B2F3D6&_nc_sid=5b3566
+
+  //Select Player two
+ // https://scontent.fagc3-2.fna.fbcdn.net/o1/v/t0/f1/m255/upload_img_67710769_08_05_2024_12_11_57_358095_7264220551358791248.jpeg?_nc_ht=scontent.fagc3-2.fna.fbcdn.net&_nc_cat=101&ccb=9-4&oh=00_AYDieoLoj_0uKG3SjrYqrXhkdYgWj-JgOiAAjtxmSiTGeA&oe=66B306F8&_nc_sid=5b3566
+// on clikc, assign hitpoints and damage? they could be the same or they could be weighted 
+// on click, pop the player's selection to the bottom area and put the other option in the opposing space.
+const mycharacter {
+
+}
+//how to assign winner for each round? hitpoints  - dmg until all is gone?
+//or random win lose, best of three?
+//or moves/cards that will affect the hit? something like rock paper scissors?
